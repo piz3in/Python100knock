5585edca4d9e50238a07b51deb3d940497d12a4e
piz3in <piz3in@gmail.com>
2023-05-31 11:59:32 +0900

    update: chapter1.pyファイルの入力データ格納フォルダ名の記載変更
    入力データのファイルを格納するフォルダ名変更に伴う記載変更

--- Part1/Chapter1/chapter1.py
+++ Part1/Chapter1/chapter1.py
@@ -6,25 +6,25 @@ import pandas as pd
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # データの読みこみ
-customer_master = pd.read_csv("01_input/customer_master.csv")
+customer_master = pd.read_csv("input/customer_master.csv")
 print("customer_master:")
 customer_master.head()
 
 # %%
-item_master = pd.read_csv("01_input/item_master.csv")
+item_master = pd.read_csv("input/item_master.csv")
 print("item_master:")
 item_master.head()
 
 # %%
-transaction_1 = pd.read_csv("01_input/transaction_1.csv")
-transaction_2 = pd.read_csv("01_input/transaction_2.csv")
+transaction_1 = pd.read_csv("input/transaction_1.csv")
+transaction_2 = pd.read_csv("input/transaction_2.csv")
 
 print("transaction:")
 transaction_1.head()
 
 # %%
-transaction_detail_1 = pd.read_csv("01_input/transaction_detail_1.csv")
-transaction_detail_2 = pd.read_csv("01_input/transaction_detail_2.csv")
+transaction_detail_1 = pd.read_csv("input/transaction_detail_1.csv")
+transaction_detail_2 = pd.read_csv("input/transaction_detail_2.csv")
 
 print("transaction_detail:")
 transaction_detail_1.head()
