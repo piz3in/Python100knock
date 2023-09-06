6cf1e8fbb3d013677cc26edca3f28ceb93138783
piz3in <piz3in@gmail.com>
2023-09-06 10:51:26 +0900

    update: ノック36のコード追加
    ノック36：翌月の利用回数予測を行うためのデータ準備

--- Part2/Chapter4/chapter4.py
+++ Part2/Chapter4/chapter4.py
@@ -104,3 +104,56 @@ customer_clustering.groupby(["cluster", "is_deleted"]).count()["customer_id"]
 customer_clustering.groupby(["cluster", "routine_flg"]).count()["customer_id"]
 
 # %%
+# 翌月の利用回数の予測を行う
+# 2018年5-10月の6ヶ月間のデータを用いて2018年11月の利用回数を予測する
+# 1.顧客毎に月毎の利用回数を集計する
+# 1-1.usedateデータをdatetime型に変換する
+use_log["usedate"] = pd.to_datetime(use_log["usedate"])
+# 1-2.monthデータ列を追加する
+use_log["usemonth"] = use_log["usedate"].dt.strftime("%Y-%m")
+use_log.head()
+# %%
+# 1-3.顧客毎の月毎の利用データを作成する
+monthly_use_log = use_log.groupby(["customer_id", "usemonth"], as_index=False).count()
+monthly_use_log.drop("usedate", axis=1, inplace=True)
+monthly_use_log.rename(columns={"log_id": "count"}, inplace=True)
+monthly_use_log.head()
+
+# %%
+# 2.顧客毎に予測月の利用回数とその過去6ヶ月の利用回数のデータセットを作成する
+# データ取得期間が2018年4月から2019年3月なので、予測月は過去6ヶ月のデータが取れる2018年10月から2019年3月までとなる
+year_months = list(monthly_use_log["usemonth"].unique())
+
+predict_data = pd.DataFrame()
+
+for i in range(6, len(year_months)):  # 予測月は過去6ヶ月のデータが取れる2018年10月から
+    # 予測月の利用回数データのデータフレーム（tmp）を作成する
+    tmp = monthly_use_log[monthly_use_log["usemonth"] == year_months[i]].copy()
+    tmp.rename(columns={"count": "count_pred", "usemonth": "pred_month"}, inplace=True)
+
+    for j in range(1, 7):
+        # 予測月の過去6ヶ月の利用回数データを各列に持つデータフレーム（tmp_before）を作成する
+        tmp_before = monthly_use_log[
+            monthly_use_log["usemonth"] == year_months[i - j]
+        ].copy()
+
+        # 当月（予測月の前の月）を0として、何ヶ月前の利用回数かを表すカラム名をつける
+        tmp_before.rename(columns={"count": f"count_{j-1}"}, inplace=True)
+
+        # カラム名でいつの月の利用回数か判別できるようになったので、不要になったusemonthデータ列を削除する
+        tmp_before.drop("usemonth", axis=1, inplace=True)
+
+        # 予測月の利用回数データに過去の利用回数データを結合する
+        tmp = pd.merge(tmp, tmp_before, how="left", on="customer_id")
+
+    # 各予測月について作成した予測月と過去の利用回数データのデータフレームを結合する
+    predict_data = pd.concat([predict_data, tmp], ignore_index=True)
+
+predict_data.head()
+
+# %%
+# 3.過去6ヶ月間のデータに欠損があるデータを削除する
+predict_data = predict_data.dropna(ignore_index=True)
+predict_data.head()
+
+# %%
