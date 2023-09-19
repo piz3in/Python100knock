c03133d3083821b8c5c81b30b0532a2768851cd0
piz3in <piz3in@gmail.com>
2023-09-19 15:15:14 +0900

    update: ノック55のコード追加
    ノック55：ルートの重みづけを行う

--- Part3/Chapter6/NetworkX.py
+++ Part3/Chapter6/NetworkX.py
@@ -1,4 +1,5 @@
 # %%
+import pandas as pd
 import networkx as nx
 
 # ネットワークを可視化する
@@ -33,5 +34,48 @@ G.add_edge("nodeA", "nodeD")
 pos["nodeD"] = (1, 0)
 
 nx.draw(G, pos, with_labels=True)
+
+# %%
+# ルートの重みづけを行う
+# 1.データの読み込み
+df_w = pd.read_csv("input/networkx/network_weight.csv")
+df_p = pd.read_csv("input/networkx/network_pos.csv")
+
+# 2.エッジの重みをリスト化する
+size = 10
+edge_weights = []
+for i in range(len(df_w)):
+    for j in range(len(df_w.columns)):
+        edge_weights.append(df_w.iloc[i, j] * size)
+
+# 3.グラフオブジェクトの作成
+G = nx.Graph()
+
+# 4.頂点の設定
+for i in range(len(df_w.columns)):
+    G.add_node(df_w.columns[i])
+
+# 5.辺の設定
+for i in range(len(df_w.columns)):
+    for j in range(len(df_w.columns)):
+        G.add_edge(df_w.columns[i], df_w.columns[j])
+
+# 6.座標の設定
+pos = {}
+for i in range(len(df_w.columns)):
+    node = df_w.columns[i]
+    pos[node] = (df_p.loc[0, node], df_p.loc[1, node])
+
+# 7.描画
+nx.draw(
+    G,
+    pos,
+    with_labels=True,
+    font_size=16,
+    node_size=1000,
+    node_color="k",
+    font_color="w",
+    width=edge_weights,
+)
 
 # %%
